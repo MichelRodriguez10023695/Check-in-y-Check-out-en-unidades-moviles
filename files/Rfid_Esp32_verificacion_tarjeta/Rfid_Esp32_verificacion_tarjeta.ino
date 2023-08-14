e455693bf6df5cf0bf0863b6f52cb5a2758f2876
MichelRodriguez10023695 <133834087+MichelRodriguez10023695@users.noreply.github.com>
2023-08-13 22:32:00 -0600

    actalizacion de evidencias

---
+++ files/Rfid_Esp32_verificacion_tarjeta/Rfid_Esp32_verificacion_tarjeta.ino
@@ -0,0 +1,111 @@
+//codigo de lectura para identificacion de ID hexadeciaml de tarjetas RFID
+#include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 5
+#define RST_PIN 22
+
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instancia de tipo de clase 
+MFRC522::MIFARE_Key key;
+byte nuidPICC[4];
+
+//------------------------------------------------
+String DatoHex;                                //datos hexadeciamles de tarjetas registradas
+const String UserReg_1 = "23B36511";
+const String UserReg_2 = "B33786A3";
+const String UserReg_3 = "7762C83B";
+//------------------------------------------------
+
+void setup() 
+{
+   Serial.begin(115200);
+   SPI.begin(); // INICIA LA COMUNICACION SPI
+   rfid.PCD_Init(); // INICIA EL MODULO RFID
+   Serial.println();
+   Serial.print(F("Reader :"));
+   rfid.PCD_DumpVersionToSerial();
+   for (byte i = 0; i < 6; i++) {
+     key.keyByte[i] = 0xFF;
+   } 
+   DatoHex = printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+   Serial.println();
+   Serial.println();
+   Serial.println("Iniciando el Programa");
+}
+
+void loop() 
+{
+     // Verificar si hay una tarjeta nueva presente en el lector
+     if ( ! rfid.PICC_IsNewCardPresent()){return;}
+     
+     // Leer los datos de la tarjeta
+     if ( ! rfid.PICC_ReadCardSerial()){return;}
+     
+     Serial.print(F("PICC type: "));
+     MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+     Serial.println(rfid.PICC_GetTypeName(piccType));
+     // verifica que sea el tipo de tarjeta admitido MIFARE
+     if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+     {
+       Serial.println("Su Tarjeta no es del tipo MIFARE Classic.");
+       return;
+     }
+     // Comparar los datos de la tarjeta con los datos almacenados
+     if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3] )
+     {
+       Serial.println("Se ha detectado una nueva tarjeta.");
+       
+       // Store NUID into nuidPICC array
+       for (byte i = 0; i < 4; i++) {nuidPICC[i] = rfid.uid.uidByte[i];}
+    
+       DatoHex = printHex(rfid.uid.uidByte, rfid.uid.size);
+       Serial.print("Codigo Tarjeta: "); Serial.println(DatoHex);
+    
+       if(UserReg_1 == DatoHex)
+       {
+        Serial.println("USUARIO 1 - PUEDE INGRESAR");     
+       }
+       else if(UserReg_2 == DatoHex)
+       {
+        Serial.println("USUARIO 2 - PUEDE INGRESAR");
+       }
+       else if(UserReg_3 == DatoHex)
+       {
+        Serial.println("USUARIO 3 - PUEDE INGRESAR");
+       }
+       else
+       {
+        Serial.println("NO ESTA REGISTRADO - PROHIBIDO EL INGRESO");
+       }  
+       Serial.println();
+     }
+     else 
+     {
+      Serial.println("Tarjeta leida previamente");
+     }
+     // Halt PICC
+     rfid.PICC_HaltA();
+     // Stop encryption on PCD
+     rfid.PCD_StopCrypto1();
+}
+
+
+String printHex(byte *buffer, byte bufferSize)
+{  
+   String DatoHexAux = "";
+   for (byte i = 0; i < bufferSize; i++) 
+   {
+       if (buffer[i] < 0x10)
+       {
+        DatoHexAux = DatoHexAux + "0";
+        DatoHexAux = DatoHexAux + String(buffer[i], HEX);  
+       }
+       else { DatoHexAux = DatoHexAux + String(buffer[i], HEX); }
+   }
+   
+   for (int i = 0; i < DatoHexAux.length(); i++) {DatoHexAux[i] = toupper(DatoHexAux[i]);}
+   return DatoHexAux;
+}
+
